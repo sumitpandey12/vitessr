15cf9898ee556d62de1cf7312d3b4df00a33176d
sumitpadney12 <heerjnv@gmail.com>
2023-12-03 03:10:08 +0530

    HomeOG

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Helmet>
+        <title>Home</title>
+        <meta property="og:url" content="www.google.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="Test Desc Home" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1682685796852-aa311b46f50d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+        />
+      </Helmet>
 
-export default Home
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default Home;
