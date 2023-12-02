7552972f531de4da9c70fe91650d2415f34abe6f
sumitpadney12 <heerjnv@gmail.com>
2023-12-03 04:26:26 +0530

    Test 11

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,17 @@ const Home = () => {
           content="https://images.unsplash.com/photo-1682685796852-aa311b46f50d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
         />
       </Helmet> */}
-      __PAGE_META__
+      <head>
+        <title>Home</title>
+        <meta property="og:url" content="www.google.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="Test Desc Home" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1682685796852-aa311b46f50d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+        />
+      </head>
       <div>Home</div>
     </div>
   );
